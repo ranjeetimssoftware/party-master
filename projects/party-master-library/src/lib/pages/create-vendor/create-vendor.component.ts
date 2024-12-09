94cb0e01700776e8072ec641d7c90c6c2eadcb31
Binit Joshi <binitjoshi4554@gmail.com>
2024-12-09 15:30:52 +0545

    formatted code

--- projects/party-master-library/src/lib/pages/create-vendor/create-vendor.component.ts
+++ projects/party-master-library/src/lib/pages/create-vendor/create-vendor.component.ts
@@ -35,6 +35,7 @@ export class CreateVendorComponent {
       Email: ['', [Validators.required,Validators.email]],
       Mobile: ['', Validators.required],
       Phone: ['', Validators.required],
+      
     });
     if (this.userSettings.CompanyType == 'B2B') {
       this.partyMasterService.customermasterObj.isCustomerLedger = 0;
@@ -76,11 +77,6 @@ export class CreateVendorComponent {
   }
 
   submit() {
-    if(!this.vendorForm.get('Email')?.valid){
-      alert('Please enter a valid email address');
-      return;
-    }
-
     if (
       this.partyMasterService.customermasterObj.customerName == '' ||
       this.partyMasterService.customermasterObj.customerName == undefined ||
@@ -124,6 +120,13 @@ export class CreateVendorComponent {
     ) {
       alert('Please Enter Mobile No.');
       return;
+    }
+    if(!this.vendorForm.get('Email')?.valid){
+      alert('Please enter a valid email address');
+      return;
+    }
+    if (!this.partyMasterService.customermasterObj.AdditionalInfo.customerStatus) {
+      this.partyMasterService.customermasterObj.AdditionalInfo.customerStatus = 'active';
     }
     // if (this.partyMasterService.customermasterObj.isCustomerLedger == 1) {
     // }
