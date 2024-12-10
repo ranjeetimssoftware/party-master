84fbcafac6ca5f76d5dbb2ea6afe86051d0be598
Binit Joshi <binitjoshi4554@gmail.com>
2024-12-10 15:31:06 +0545

    .

--- projects/party-master-library/src/lib/config.service.ts
+++ projects/party-master-library/src/lib/config.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConfigService {
-  private apiUrl:string = 'http://192.168.126.88:819/publish/api/v1';
+  private apiUrl:string = 'http://202.51.74.144:831/WEBACCOUNT-DYNAMICMENU/api/v1';
 
   // Initialize with default URL or environment-based config if available
   setApiUrl(url: string): void {
